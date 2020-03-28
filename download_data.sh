11d2cc20d2e977b609a6f86bbd226965364ca632
Rajat Agrawal <rajatag1604@gmail.com>
2020-03-28 21:29:16 +0000

    downloading the demo test data to full_data

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,8 @@ wget https://nlpcrossworddata.blob.core.windows.net/test2/data-20200318T221609Z-
 unzip data-20200318T221609Z-001.zip
 cp guardian_data/test_guardian_600.tok data/full_data
 
+wget https://raw.githubusercontent.com/ucabops/robbie/master/data/gquick-10-entries.txt -P data/full_data
+
 cd data/embeddings
 wget https://nlpcrossworddata.blob.core.windows.net/test2/glove.6B.zip
 unzip glove.6B.zip
